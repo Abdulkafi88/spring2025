46277fbf713b39aeaf25f63356b61f118ad29bcd
Abdulkafi Nirig <abdulkafinirig@Mac.local.tld>
2025-05-19 13:20:31 -0700

    i made a json fill with all info about my fave player in the nba

--- pythonwork.py
+++ pythonwork.py
@@ -1,17 +1,17 @@
-import jason
-
-
-
-
+import json
 
 data = { 
-
-    "name" : "paul george", 
-    "agae" : 35 ,
-    "team" : "Philadelphia 76ers ",
-    "All-Start" : 9, 
-    "All-Nba Team" : 6, 
-    
-
-
+    "name": "paul george", 
+    "age": 35,
+    "team": "Philadelphia 76ers",
+    "All-Star": 9, 
+    "All-NBA Team": 6, 
+    "injured": True
 }
+
+with open("data.json", "w") as json_file:
+    json.dump(data, json_file, indent=4)
+
+
+print("Data has been written to data.json")
+   
