afa6447dcefa76bb4b3f4aa6d35e5e03e1670df4
Abdulkafi Nirig <abdulkafinirig@Mac.local.tld>
2025-05-19 12:58:16 -0700

    first commit

---
+++ pythonwork.py
@@ -0,0 +1,17 @@
+import jason
+
+
+
+
+
+data = { 
+
+    "name" : "paul george", 
+    "agae" : 35 ,
+    "team" : "Philadelphia 76ers ",
+    "All-Start" : 9, 
+    "All-Nba Team" : 6, 
+    
+
+
+}
